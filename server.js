e10939efab955f17c1166e4f5c4016662d279367
Ritchie Martori <skawful@gmail.com>
2012-04-10 17:24:00 -0700

    light theme

--- server.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var app = express.createServer();
-app.use(express.static(__dirname + '/docs'));
-
-app.get('/', function(req, res){
-  res.redirect('/index.html');
-});
-
-app.listen(8000);
